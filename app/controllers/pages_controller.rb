a9115d6697c344958dcfd2db14e0650212dbe90c
nicolas nguon <nicolas.nguon@checkmyguest.fr>
2022-07-16 10:03:37 +0200

    exo

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def about
+    @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
+  end
+
+  def contact
+
+  end
+
+end
